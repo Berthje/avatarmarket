b83325c95a2c370aa55b67afb7928fad841c09da
layton.berth <layton.berth@student.howest.be>
2022-12-09 17:15:49 +0100

    navbar finished

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,6 +5,7 @@ import { navLinks } from "../utils/data";
 import { RiUserLine, RiMenuLine } from "react-icons/ri";
 import { FiMinimize, FiSearch } from "react-icons/fi";
 import { FaReddit, FaTelegramPlane, FaDiscord } from "react-icons/fa";
+import { TbWallet } from "react-icons/tb";
 
 const Navbar = () => {
   const router = useRouter();
@@ -18,13 +19,51 @@ const Navbar = () => {
           avatarmarket
         </h2>
       </div>
+      <div className="hidden xl:flex items-center justify-between w-full px-24">
+        {/* Search Bar */}
+        <form className="relative w-2/4">
+          <FiSearch className="absolute top-2/4 left-2 -translate-y-2/4" />
+          <input
+            type="search"
+            name="searchquery"
+            id="searchquery"
+            placeholder="Search"
+            className="bg-[#D9D9D9]/20 rounded-md py-2 px-8 outline-none border-none w-full"
+          />
+        </form>
+        {/* Menu */}
+        <nav className="w-3/5 flex justify-end">
+          <ul className="flex">
+            {navLinks.map((link, index) => {
+              const activeStyle = link.path === router.pathname ? "active" : "";
+              return (
+                <li
+                  key={index}
+                  className={`font-[CalSans] ${activeStyle} ml-5`}
+                >
+                  <Link href={link.path} className="hover:text-[#785ACE]">
+                    {link.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
       {/* Right Part (navigation) */}
       <div className="flex items-center">
+        {/* Connect Wallet Account Page */}
+        <div className="bg-white/25 rounded-full p-3 mr-3">
+          <TbWallet className="text-xl" />
+        </div>
         {/* Circle Account Page */}
         <div className="bg-white/25 rounded-full p-3 mr-6">
           <RiUserLine className="text-xl" />
         </div>
-        <RiMenuLine onClick={() => setNav(!nav)} className="text-2xl" />
+        <RiMenuLine
+          onClick={() => setNav(!nav)}
+          className="xl:hidden text-2xl"
+        />
       </div>
       {/* Mobile Menu */}
       <div
