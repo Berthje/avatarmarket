91cc6c7238761fe73b6aec673783033bbea439e1
layton.berth <layton.berth@student.howest.be>
2022-12-26 23:20:46 +0100

    Fixed Rendering collection images

--- components/Collection.jsx
+++ components/Collection.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 
 const Collection = ({
   name,
@@ -7,20 +6,24 @@ const Collection = ({
   previewImage1,
   previewImage2,
   previewImage3,
-  previewImage4,
 }) => {
   return (
     <div className="pb-12 max-w-[320px]">
       <div className="w-full h-full grid grid-cols-2 gap-2">
-        <div className='w-[155px] h-[105px] overflow-hidden bg-[url("/collectionImages/foustling1.png")]'></div>
-        <div className='w-[155px] h-[105px] overflow-hidden bg-[url("/collectionImages/foustling1.png")]'></div>
-        <div className='w-[155px] h-[105px] overflow-hidden bg-[url("/collectionImages/foustling1.png")]'></div>
-        <div className='w-[155px] h-[105px] overflow-hidden bg-[url("/collectionImages/foustling1.png")]'></div>
+        <div className='w-[155px] h-[105px] bg-cover bg-center' style={{backgroundImage: `url(${previewImage1.substring(previewImage1.indexOf('/')+1)})`}}></div>
+        <div className='w-[155px] h-[105px] bg-cover bg-center' style={{backgroundImage: `url(${previewImage2.substring(previewImage1.indexOf('/')+1)})`}}></div>
+        <div className='w-[155px] h-[105px] bg-cover bg-center' style={{backgroundImage: `url(${previewImage3.substring(previewImage1.indexOf('/')+1)})`}}></div>
+        <div className='w-[155px] h-[105px] bg-[length:500%] bg-left' style={{backgroundImage: `url(${previewImage3.substring(previewImage1.indexOf('/')+1)})`}}></div>
+      </div>
+      <div className="w-[320px] h-[110px] bg-[#E147FD] rounded-b-[30px] absolute -z-[100] mt-[9px]"></div>
+      <div className="w-full h-[110px] bg-[#1A1A1A] rounded-b-[30px] flex justify-between items-center px-10 mt-2">
+        <h2 className="font-[CalSans] text-[1.1rem] tracking-wider">{name}</h2>
+        <div className="border-2 border-[#878787] rounded-md p-1.5">
+          <h3 className="font-[Poppins] text-[0.8rem] text-[#878787] font-bold">{items} items</h3>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Collection;
-
-//<img src={previewImage1} className='-mt-10'></img>
--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Image from "next/image";
-import cover from '../public/collectionImages/foustling1.png'
 import Collection from '../components/Collection';
+
+import foustling_img1 from '../public/collectionImages/foustling1.png';
+import foustling_img2 from '../public/collectionImages/foustling2.png';
+import foustling_img3 from '../public/collectionImages/foustling3.png';
 
 const Trending = () => {
   const collections = [
-    {id: 1, name: "Foustlings", items: 1750, previewImage1: cover, previewImage2: cover, previewImage3: cover, previewImage4: cover},
-    {id: 2, name: "Foustlings", items: 1750, previewImage1: cover, previewImage2: cover, previewImage3: cover, previewImage4: cover},
-    {id: 3, name: "Foustlings", items: 1750, previewImage1: cover, previewImage2: cover, previewImage3: cover, previewImage4: cover},
-    {id: 4, name: "Foustlings", items: 1750, previewImage1: cover, previewImage2: cover, previewImage3: cover, previewImage4: cover},
+    {id: 1, name: "Foustlings", items: 1750, previewImage1: foustling_img1, previewImage2: foustling_img2, previewImage3: foustling_img3},
+    {id: 2, name: "Foustlings", items: 1750, previewImage1: foustling_img1, previewImage2: foustling_img2, previewImage3: foustling_img3},
+    {id: 3, name: "Foustlings", items: 1750, previewImage1: foustling_img1, previewImage2: foustling_img2, previewImage3: foustling_img3},
+    {id: 4, name: "Foustlings", items: 1750, previewImage1: foustling_img1, previewImage2: foustling_img2, previewImage3: foustling_img3},
   ];
 
   return (
@@ -16,7 +19,7 @@ const Trending = () => {
       <h2 className='flex justify-center items font-[CalSans] text-[1.8rem] mb-8'><Image src="/flame.png" alt='flame' width={40} height={40} className='mr-4'/>Trending Collections</h2>
       <div className='w-80 m-auto'>
         {collections.map(collection => (
-          <Collection name={collection.name} items={collection.items} previewImage1={collection.previewImage1.src} previewImage2={collection.previewImage2.src} previewImage3={collection.previewImage3.src} previewImage4={collection.previewImage4.src} key={collection.id}/>
+          <Collection name={collection.name} items={collection.items} previewImage1={collection.previewImage1.src} previewImage2={collection.previewImage2.src} previewImage3={collection.previewImage3.src} key={collection.id}/>
         ))}
       </div>
     </div>
